b07f2d1604327a58c9508793ab80f96c6afbd328
phamcuong133421 <cuongpham133421pcc@gmail.com>
2022-09-06 15:27:56 +0700

    tai lieu bo sung

--- bai_4/1_document.py
+++ bai_4/1_document.py
@@ -4,3 +4,7 @@ https://stackabuse.com/numpy-tutorial-a-simple-example-based-guide/
 '''
 https://www.tutorialspoint.com/numpy/index.htm
 '''
+#tài liệu bổ sung
+'''
+https://docs.scipy.org/doc/numpy-1.15.1/user/quickstart.html#prerequisites
+'''
